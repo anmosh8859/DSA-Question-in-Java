d5af1b7eae6d9cca032ab08177d98a806c168f4e
anmosh8859 <anandmsharma786@gmail.com>
2023-12-30 06:55:28 +0530

    LeetCode2367ArithmeticTriplet class added

---
+++ LeetCode2367ArithmeticTriplet.java
@@ -0,0 +1,19 @@
+public class LeetCode2367ArithmeticTriplet {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int total = 0;
+        for(int i = 0; i < nums.length-2 ; i++){
+            int  j = i+1;
+            int k = nums.length-1;
+            while((j<k) && (nums[j] - nums[i] != diff)){
+                j++;
+            }
+            while((k>j) && (nums[k] - nums[j] != diff)){
+                k--;
+            }
+            if((nums[j] - nums[i] == diff) && (nums[k] - nums[j] == diff)){
+                total++;
+            }
+        }
+        return total;
+    }
+}
--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
 //        LargeIntegerSum lis = new LargeIntegerSum();
 //        lis.lis(a,b);
 
-//        int [] arr  = {3,5,7,1,4,2,6};
+        int [] arr  = {0,1,4,6,7,10};
 //        RotateArray.intermediateArray(arr,3);
 //        RotateArray.bubbleRotate(arr,3);
 //        RotateArray.reverseRotate(arr,3);
@@ -65,7 +65,9 @@ public class Main {
 //        System.out.println(fm.findMedianSortedArrays(nums1,nums2));
 //        String s = "pwwkew";
 //        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring(s));
-        int [] plants = {2,3,5,9,64};
-        System.out.println(new LeetCode2105WateringThePlants().minimumRefill(plants,4,5));
+//        int [] plants = {2,3,5,9,64};
+//        System.out.println(new LeetCode2105WateringThePlants().minimumRefill(plants,4,5));
+        System.out.println(new LeetCode2367ArithmeticTriplet().arithmeticTriplets(arr,3));
+
     }
 }
